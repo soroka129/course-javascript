cf03d8114900e96aab47250ded7f89c68c2143ec
Sorokin Evgeniy <soroka129@mail.ru>
2024-02-16 21:01:41 +0700

    Выполненое ДЗ неделя 1

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,7 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
+function returnFirstArgument(a) {
+  return a;
+}
 
 /*
  Задание 2:
@@ -28,7 +30,9 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults() {}
+function sumWithDefaults(a, b = 100) {
+  return a + b;
+}
 
 /*
  Задание 3:
@@ -38,7 +42,13 @@ function sumWithDefaults() {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult() {}
+function returnFnResult(Fn) {
+  return Fn();
+}
+
+returnFnResult(function Fn(a) {
+  return a;
+});
 
 /*
  Задание 4:
@@ -53,7 +63,13 @@ function returnFnResult() {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter() {}
+function returnCounter(n = 0) {
+  function F() {
+    n++;
+    return n;
+  }
+  return F;
+}
 
 /*
  Задание 5 *:
@@ -64,7 +80,10 @@ function returnCounter() {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+
+function returnArgumentsArray(...numbers) {
+  return numbers;
+}
 
 export {
   returnFirstArgument,
